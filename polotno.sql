bc2c4932fcb884c5aad26663dd59271b9fe37a29
lisakaminska <146975018+lisakaminska@users.noreply.github.com>
2024-12-03 11:09:03 +0200

    fixed some tables in polotno.sql

--- polotno.sql
+++ polotno.sql
@@ -1,8 +1,7 @@
--- Створення бази даних
 CREATE DATABASE IF NOT EXISTS Polotno;
 USE Polotno;
 
--- Таблиця Users
+
 CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
@@ -11,24 +10,24 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
--- Таблиця ArtMovements
+
 CREATE TABLE IF NOT EXISTS art_movement (
     movement_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    description TEXT NOT NULL
+    movement_name VARCHAR(50) NOT NULL UNIQUE,
+    movement_description TEXT NOT NULL
 );
 
--- Таблиця Genres
+
 CREATE TABLE IF NOT EXISTS genre (
     genre_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    description TEXT NOT NULL
+    genre_name VARCHAR(50) NOT NULL UNIQUE,
+    genre_description TEXT NOT NULL
 );
 
--- Таблиця Artists
+
 CREATE TABLE IF NOT EXISTS artist (
     artist_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    artist_name VARCHAR(100) NOT NULL,
     date_of_birth INT NOT NULL,
     date_of_death INT,
     place_of_birth TEXT NOT NULL,
@@ -39,21 +38,21 @@ CREATE TABLE IF NOT EXISTS artist (
     FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
 );
 
--- Таблиця Paintings
+
 CREATE TABLE IF NOT EXISTS painting (
     painting_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    painting_name VARCHAR(100) NOT NULL,
     artist_id INT NOT NULL,
     style_id INT,
     genre_id INT,
-    year INT,
-    description TEXT,
+    year_created INT,
+    painting_description TEXT,
     FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
     FOREIGN KEY (style_id) REFERENCES art_movement(movement_id),
     FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
 );
 
--- Таблиця Favorites
+
 CREATE TABLE IF NOT EXISTS favorite (
     user_id INT NOT NULL,
     painting_id INT NOT NULL,
@@ -63,24 +62,24 @@ CREATE TABLE IF NOT EXISTS favorite (
     FOREIGN KEY (painting_id) REFERENCES painting(painting_id)
 );
 
--- Таблиця Tests
+
 CREATE TABLE IF NOT EXISTS Test (
     test_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    test_name VARCHAR(100) NOT NULL,
     difficulty INT DEFAULT 1,
     artist_id INT,
-    FOREIGN KEY (artist_id) REFERENCES artis(artist_id)
+    FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 );
 
--- Таблиця Questions
+
 CREATE TABLE IF NOT EXISTS question (
     question_id INT AUTO_INCREMENT PRIMARY KEY,
     test_id INT NOT NULL,
     question_text TEXT NOT NULL,
-    FOREIGN KEY (test_id) REFERENCES tests(test_id)
+    FOREIGN KEY (test_id) REFERENCES test(test_id)
 );
 
--- Таблиця Answers
+
 CREATE TABLE IF NOT EXISTS answer (
     answer_id INT AUTO_INCREMENT PRIMARY KEY,
     question_id INT NOT NULL,
@@ -89,7 +88,7 @@ CREATE TABLE IF NOT EXISTS answer (
     FOREIGN KEY (question_id) REFERENCES question(question_id)
 );
 
--- Таблиця UserTestResults
+
 CREATE TABLE IF NOT EXISTS user_test_result (
     result_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -99,3 +98,4 @@ CREATE TABLE IF NOT EXISTS user_test_result (
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (test_id) REFERENCES test(test_id)
 );
+
