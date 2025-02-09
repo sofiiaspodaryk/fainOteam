8c685ae09b1939be4d6d5e29c34f3cd07d305009
roman-stozhuk <Roman.Stozhuk.ShI.2023@lpnu.ua>
2025-02-09 12:18:45 +0200

    Covered UserController.AddUser() with tests

--- backend/Polotno.Tests/Controllers/UserControllerTests.cs
+++ backend/Polotno.Tests/Controllers/UserControllerTests.cs
@@ -67,4 +67,74 @@ public class UserControllerTests
             .Should().BeOfType<NotFoundObjectResult>()
             .Which.Value.Should().BeEquivalentTo(expectedMessage);
     }
+
+    [Test]
+    public async Task AddUser_ReturnsBadRequest_WhenModelStateIsInvalid()
+    {
+        // Arrange
+        var validDto = new AddRequestUserDto
+        {
+            Username = "testuser",
+            Email = "example@gmail.com",
+            Password = "123456789"
+        };
+
+        _controller.ModelState.AddModelError("Password", "Password is required");
+
+        // Act
+        var result = await _controller.AddUser(validDto);
+
+        // Assert
+        result
+            .Should().BeOfType<BadRequestObjectResult>()
+            .Which.Value.Should().BeOfType<SerializableError>();
+    }
+
+    [Test]
+    public async Task AddUser_ReturnsOk_WhenModelStateIsValid()
+    {
+        // Arrange
+        var validRequestDto = new AddRequestUserDto
+        {
+            Username = "testuser",
+            Email = "example@gmail.com",
+            Password = "123456789"
+        };
+
+        var user = new User
+        {
+            Username = validRequestDto.Username,
+            Email = validRequestDto.Email,
+            PasswordHash = validRequestDto.Password
+        };
+
+        var userDto = new UserDto
+        {
+            Username = validRequestDto.Username,
+            Email = validRequestDto.Email
+        };
+
+        var expectedMessage = new { message = "User added successfully", userDto };
+
+        _mockMapper
+            .Setup(map => map.Map<User>(validRequestDto))
+            .Returns(user);
+
+        _mockRepo
+            .Setup(repo => repo.AddAsync(user))
+            .ReturnsAsync(user);
+
+        _mockMapper
+            .Setup(map => map.Map<UserDto>(user))
+            .Returns(userDto);
+
+        // Act
+        var result = await _controller.AddUser(validRequestDto);
+
+        // Assert
+        result
+            .Should().BeOfType<OkObjectResult>()
+            .Which.Value.Should().BeEquivalentTo(expectedMessage);
+    }
+
 }
