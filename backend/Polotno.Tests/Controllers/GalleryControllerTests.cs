16fbd57a603314ada991b3946a473f86e6cbcd19
roman-stozhuk <Roman.Stozhuk.ShI.2023@lpnu.ua>
2025-02-15 21:49:56 +0200

    Covered GalleryController.GetAllPaintings() with tests

--- backend/Polotno.Tests/Controllers/GalleryControllerTests.cs
+++ backend/Polotno.Tests/Controllers/GalleryControllerTests.cs
@@ -61,4 +61,49 @@ public class GalleryControllerTests
             .And.BeOfType<NotFoundObjectResult>()
             .Which.Value.Should().BeEquivalentTo(expectedMessage);
     }
+
+    [Test]
+    public async Task GetAllPaintings_ReturnsOk_WhenThereArePaintings()
+    {
+        // Arrange
+        var expectedPaintings = new List<PaintingDto> {
+            new PaintingDto {
+                PaintingId = 1,
+                PaintingName = "Starry Night"
+            }
+        };
+
+        _mockRepo
+            .Setup(repo => repo.GetAllAsync(null, null, "Night", null, null))
+            .ReturnsAsync(expectedPaintings);
+
+        // Act
+        var result = await _controller.GetAllPaintings(null, null, "Night", null, null);
+
+        // Assert
+        result
+            .Should().BeOfType<OkObjectResult>()
+            .Which.Value.Should().BeEquivalentTo(expectedPaintings);
+    }
+
+    [Test]
+    public async Task GetAllPaintings_ReturnsNotFiund_WhenFilteringGivesNoResults()
+    {
+        List<PaintingDto> EmptyGallery = Enumerable.Empty<PaintingDto>().ToList();
+
+        var expectedMessage = new { message = "Paintings not found" };
+
+        _mockRepo
+            .Setup(repo => repo.GetAllAsync(-1, 0, null, null, null))
+            .ReturnsAsync(EmptyGallery);
+
+        // Act
+        var result = await _controller.GetAllPaintings(-1, 0, null, null, null);
+
+        // Assert
+        result
+            .Should().NotBeNull()
+            .And.BeOfType<NotFoundObjectResult>()
+            .Which.Value.Should().BeEquivalentTo(expectedMessage);
+    }
 }
