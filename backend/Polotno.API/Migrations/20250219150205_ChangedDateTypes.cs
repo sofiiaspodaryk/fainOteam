f3de423bc05392489efea69602e21940fb3925a9
yaroslav-vasylyshyn <146115906+yaroslav-vasylyshyn@users.noreply.github.com>
2025-02-19 17:42:06 +0200

    changed artist dates type
    changed DateOfBirth and DateOfDeath to DateTime

---
+++ backend/Polotno.API/Migrations/20250219150205_ChangedDateTypes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Polotno.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedDateTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "date_of_death",
+                table: "artist",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "date_of_birth",
+                table: "artist",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "date_of_death",
+                table: "artist",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "date_of_birth",
+                table: "artist",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+        }
+    }
+}
